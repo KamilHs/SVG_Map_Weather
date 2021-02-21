4798d461a8700fe24680d5b9db96aec686080524
KamilHs <kamilsalimli1940@gmail.com>
2021-02-21 14:30:50 +0400

    api removed

--- api/queries/delete.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-const DELETE_RECORD = "DELETE FROM records WHERE record_id=:record_id";
--- api/queries/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once("get.inc.php");
-require_once("post.inc.php");
-require_once("delete.inc.php");
-
-
